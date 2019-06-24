00d85fea6ba8cc8fbf65d894abad28ed43a03f10
João Victor Ferreira de Andrade <shaqit7474@gmail.com>
2019-06-23 23:00:46 -0300

    Update estoquecontrol.py

--- estoquecontrol.py
+++ estoquecontrol.py
@@ -1,26 +1,29 @@
 pro = [("789", "4009")]
 produtos = dict(pro)
 produtos.pop("789")
-print()
-print()
-print()
-print("Cadastro de Produtos")
-print()
-print()
-print("[1] Cadastrar Produtos")
-print("[2] Consultar Produtos")
-print("[3] Sair")
-print()
-print()
-
-op1 = int(input("Opção Desejada"))
-
+op1=0
 while op1 !=3:
-
-    if op1 == 1:
-        insert = input("Código")
-        produtos[insert] = float(input("Valor"))
-
+    print("\n"*500)
+    print("Cadastro de Produtos")
+    print("[1] Cadastrar Produtos")
+    print("[2] Consultar Produtos")
+    print("[3] Sair")
+    p1 = int(input("Opção Desejada"))
+    if op1 ==1:
+        opcao = 0
+        while opcao !=2:
+            print("\n"*500)
+            insert = input("Código")
+            produtos[insert] = float(input("Valor"))
+            print("\n"*500)
+            print("[1] Continuar a cadastrar")
+            print("[2] Sair e Salvar")
+            print()
+            opcao = int(input("Opção:"))
+            print("\n"*500)
     elif op1 == 2:
+        print("\n"*500)
         print(produtos)
+        opcao02 = input("Enter para Sair")
+
     
